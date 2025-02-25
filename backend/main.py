e05898dbfc0b93ccf301059b0b157cbe06d0e330
Lucas dechavanne <ldechavanne@outlook.fr>
2025-02-25 12:29:44 +0100

    evidently configuration

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, File, UploadFile
-from PIL import Image
+from evidently.dashboard import Dashboard
+from evidently.metrics import RegressionQualityMetric
 import io
 import numpy as np
+from PIL import Image
+from prometheus_client import start_http_server, Gauge
 
 app = FastAPI()
+
+# Initialisation des métriques Prometheus
+model_metric = Gauge('model_predictions', 'Number of predictions made by the model')
+
+# Exemple de Dashboard Evidently
+dashboard = Dashboard(metrics=[RegressionQualityMetric()])  # a voir en fonction du model
 
 @app.get("/")
 def read_root():
@@ -13,14 +22,16 @@ def read_root():
 async def predict(file: UploadFile = File(...)):
     # Lire l'image en bytes
     image_bytes = await file.read()
-    
+
     # Convertir en objet PIL
     image = Image.open(io.BytesIO(image_bytes))
-
-    # Convertir en NumPy array pour un modèle de deep learning
     image_array = np.array(image)
 
-    # Simuler une prédiction (ex: retourner la forme de l'image)
+    prediction = image_array.shape  # predict
+
+    # Mettre à jour les métriques Prometheus
+    model_metric.inc()
+
     return {
         "filename": file.filename,
         "format": image.format,
@@ -28,3 +39,6 @@ async def predict(file: UploadFile = File(...)):
         "size": image.size,
         "shape": image_array.shape
     }
+
+
+start_http_server(8001)  # port de scrapping metrics
